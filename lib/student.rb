6a900d343ac5f8da95a48773b4d13a1cbed8fec3
Lucy Wall <lhwall@gmail.com>
2018-05-15 15:40:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,13 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  attr_accessor :name, :grade 
+  attr_reader: :id 
+  
+  def initialize
+  end 
+  
+  def self.create_table
+  end 
   
 end
